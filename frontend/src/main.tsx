ac3b7bb730ec64e3d7818d59b4ad7e75bb8d22b7
shivamgiri-sudo <shivam.giri@teammas.in>
2026-06-16 02:50:24 +0530

    Switch app entry to complete enterprise console v2

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import EnterpriseConsole from "./EnterpriseConsole";
+import EnterpriseConsoleV2 from "./EnterpriseConsoleV2";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <EnterpriseConsole />
+    <EnterpriseConsoleV2 />
   </React.StrictMode>
 );
